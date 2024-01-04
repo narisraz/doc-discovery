d24d2423fbd065d1b8553e563aa54e97bd54b65d
Naris Razafimahatratra <naris.razafimahatratra@gmail.com>
2024-01-04 16:32:12 +0300

    🐛 Home page to statefull widget

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -5,11 +5,16 @@ import 'package:docdiscovery/presentation/signup/practitioner_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Home extends ConsumerWidget {
+class Home extends ConsumerStatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeState();
+}
+
+class _HomeState extends ConsumerState<Home> {
+  @override
+  Widget build(BuildContext context) {
     final auth = ref.read(getConnectedUserUseCaseProvider).execute();
     return Scaffold(
         body: const SearchPractitioner(),
